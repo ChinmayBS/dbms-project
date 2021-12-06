8f123d6d01bc030bc4f89bbe7a19f0fc59b74e81
chinmay <chinmaybs89@gmail.com>
2021-12-06 10:24:23 +0530

    initial setup

---
+++ frontend/src/HomePage.js
@@ -0,0 +1,17 @@
+import "./HomePage.css";
+
+export default function HomePage() {
+  return (
+    <div className="home">
+      <div class="content">
+        <h1 className="primary-heading">Love Street Foods</h1>
+        <p className="primary-content">
+          Find Amazing street foods on this site.
+        </p>
+        <button className="btn btn--signup">Signup</button>
+        <button className="btn btn--login">Login</button>
+      </div>
+      <div></div>
+    </div>
+  );
+}

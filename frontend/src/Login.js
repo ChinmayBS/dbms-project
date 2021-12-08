af6f089ff32fdeb6e5274e8357a158bc28c6a4df
chinmay <chinmaybs89@gmail.com>
2021-12-08 12:40:37 +0530

    add login and signup page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,3 +1,69 @@
-export default function Login() {
-  return <p>login Page</p>;
+import "./Login.css";
+import React, { useState } from "react";
+export default function Login(props) {
+  // Declare a new state variable, which we'll call "count"
+  const [toggleClass, setClass] = useState("login--container");
+
+  const changeToChefSignup = (params) => {
+    setClass("login--container");
+  };
+
+  const changetoCustomerSignup = (params) => {
+    setClass("login--container right-panel-active");
+  };
+
+  return (
+    <div className="body">
+      <div className={toggleClass} id="container">
+        <div className="form-container sign-up-container">
+          <form action="#">
+            <h1>Sign In for Customer</h1>
+
+            <input type="text" placeholder="Name" />
+            <input type="password" placeholder="Password" />
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="form-container sign-in-container">
+          <form action="#">
+            <h1>Sign In for Shop Keeper</h1>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome back, Chef</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={changeToChefSignup}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Welcome back, Food Lover!</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                className="ghost"
+                id="signUp"
+                onClick={changetoCustomerSignup}
+              >
+                Sign In
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,3 +1,62 @@
-export default function Signup() {
-  return <p>signup Page</p>;
+import "./SignUp.css";
+import React, { useState } from "react";
+export default function Signup(props) {
+  // Declare a new state variable, which we'll call "count"
+  const [toggleClass, setClass] = useState("signup--container");
+
+  const changeToChefSignup = (params) => {
+    setClass("signup--container");
+  };
+
+  const changetoCustomerSignup = (params) => {
+    setClass("signup--container right-panel-active");
+  };
+
+  return (
+    <div className={toggleClass} id="container">
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1>Sign Up for Customer</h1>
+
+          <input type="text" placeholder="Name" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1>Sign Up for Shop Keeper</h1>
+          <input type="text" placeholder="Username" />
+
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="password" placeholder="Password" />
+
+          <button>Sign Up</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Hello, Chef</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button className="ghost" id="signIn" onClick={changeToChefSignup}>
+              Sign Up
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Food Lover!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button
+              className="ghost"
+              id="signUp"
+              onClick={changetoCustomerSignup}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
